e1101a2007609a109dbc1674c4f67c5ca2edd564
Sebastián Chamena <sebachamena@gmail.com>
2020-05-17 19:30:38 -0300

    Add utilities to check env

--- envconf.go
+++ envconf.go
@@ -10,6 +10,22 @@ const (
 
 type env string
 
+func InProduction() bool {
+	return GetENV() == Production
+}
+
+func InDevelopment() bool {
+	return GetENV() == Development
+}
+
+func InStaging() bool {
+	return GetENV() == Staging
+}
+
+func NotInDevelopment() bool {
+	return InProduction() || InStaging()
+}
+
 func GetENV() env {
 	osEnv := os.Getenv("ENV")
 	if osEnv != "" {
